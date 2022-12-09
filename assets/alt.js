535bc6ca3b16f8944d41c52d613887eab5e3f8fb
tommysoprano <thomasjaymaly@gmail.com>
2022-12-08 21:12:37 -0500

    broke some things, fixed some others

---
+++ assets/alt.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+
+
+var foundWeather = []
+
+function getWeather() {
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=e72f31e0a34db7aed5d48ea56c89d35d`;
+    $.ajax({
+        url: weatherURL,
+        method: "GET",
+    }).then(function(res){
+        for (i=0; i<6; i++) [
+            foundWeather[i].date = res.list[i].dt_txt,
+            foundWeather[i].temp = res.list[i].main.temp,
+            foundWeather[i].icon = res.list[i].weather[0].icon,
+            foundWeather[i].wind = res.list[i].wind.speed,
+            foundWeather[i].humidity = res.list[i].main.humidity
+        ]
+    })
+}
+
+getWeather("Newark")
+console.log(foundWeather)
+
+function forecast() {
+    for (i=0; i<foundWeather.length; i++) {
+        $(`#day-${i+1}`).children()
+    }
+}
