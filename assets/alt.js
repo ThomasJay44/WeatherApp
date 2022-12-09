28b5f6e8e255cfb79c3f9151663f925791164f60
tommysoprano <thomasjaymaly@gmail.com>
2022-12-08 21:16:34 -0500

    added array for holding data

--- assets/alt.js
+++ assets/alt.js
@@ -1,33 +1,71 @@
 
-
-
-
-
-
-
-var foundWeather = []
+var foundWeather = [
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+  {
+    date: "",
+    temp: "",
+    icon: "",
+    wind: "",
+    humidity: "",
+  },
+];
 
 function getWeather() {
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=e72f31e0a34db7aed5d48ea56c89d35d`;
-    $.ajax({
-        url: weatherURL,
-        method: "GET",
-    }).then(function(res){
-        for (i=0; i<6; i++) [
-            foundWeather[i].date = res.list[i].dt_txt,
-            foundWeather[i].temp = res.list[i].main.temp,
-            foundWeather[i].icon = res.list[i].weather[0].icon,
-            foundWeather[i].wind = res.list[i].wind.speed,
-            foundWeather[i].humidity = res.list[i].main.humidity
-        ]
-    })
+  var queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=e72f31e0a34db7aed5d48ea56c89d35d`;
+  $.ajax({
+    url: weatherURL,
+    method: "GET",
+  }).then(function (res) {
+    for (i = 0; i < 6; i++)
+      [
+        (foundWeather[i].date = res.list[i].dt_txt),
+        (foundWeather[i].temp = res.list[i].main.temp),
+        (foundWeather[i].icon = res.list[i].weather[0].icon),
+        (foundWeather[i].wind = res.list[i].wind.speed),
+        (foundWeather[i].humidity = res.list[i].main.humidity),
+      ];
+  });
 }
 
-getWeather("Newark")
-console.log(foundWeather)
+getWeather("Newark");
+console.log(foundWeather);
 
 function forecast() {
-    for (i=0; i<foundWeather.length; i++) {
-        $(`#day-${i+1}`).children()
-    }
+  for (i = 0; i < foundWeather.length; i++) {
+    $(`#day-${i + 1}`).children();
+  }
 }
